1d96d68c94eaff5b0a2d2c48d6da859756114d4b
scott <scoybanister@me.com>
2024-02-06 14:00:41 -0700

    fix exit code
    plz work

--- nekrosis/core.py
+++ nekrosis/core.py
@@ -303,7 +303,7 @@ def main():
                     nekrosis.install()
     else:
             parser.print_help()
-            sys.exit(1)
+            sys.exit(0)
 
 if __name__ == "__main__":
     main()
